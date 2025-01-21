3f2b89b390ac0eee2f430d755b865a715db4471d
Henrique Naldy <Henrique.nordy@gmail.com>
2025-01-21 08:34:40 -0300

    new reposit

---
+++ src/drivers/interfaces/driver_handler_interface.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+class DriverHandlerInterface(ABC):
+
+    @abstractmethod
+    def standard_derivation(self, numbers: List[float]) -> float:
+        pass
+
+    @abstractmethod
+    def variance(self, numbers: List[float]) -> float:
+        pass
